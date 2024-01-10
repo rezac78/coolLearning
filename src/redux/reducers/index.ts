7dbcdec67323411540368bedbfd2dcd58452ddac
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-01-10 23:21:03 +0330

    remove redux

--- src/redux/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import themeReducer from './themeReducer';
-
-const rootReducer = combineReducers({
-  theme: themeReducer,
-  // other reducers
-});
-
-export default rootReducer;
--- src/redux/store.ts
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-
-const store = createStore(rootReducer);
-
-export type RootState = ReturnType<typeof rootReducer>;
-export default store;
