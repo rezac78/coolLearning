59e3105beb007928610ce44a7cf2ed12bc58ee19
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-01-26 22:42:07 +0330

    ok

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://cool-api-75mo.onrender.com/api",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
