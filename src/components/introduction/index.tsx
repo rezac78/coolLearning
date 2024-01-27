db1fbc932d855fb60e97268d3fe20bc641a19cd8
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-01-27 14:59:38 +0330

    Effect home

--- src/components/introduction/index.tsx
+++ src/components/introduction/index.tsx
@@ -1,13 +1,21 @@
 import  Router  from 'next/router';
 import Button from '../Shared/Button/Button';
 import ImagePart from '../Shared/ImgPart/Image';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 export default function Introduction() {
+        useEffect(() => {
+                AOS.init({
+                        duration: 1000,
+                });
+        }, []);
         return (
                 <div className="container mx-auto w-11/12 md:w-5/6 flex flex-col md:flex-row items-center my-52">
-                        <div className="flex-1 mb-4 md:mb-0">
+                        <div className="flex-1 mb-4 md:mb-0" data-aos="fade-up">
                                 <ImagePart Src="/feature.svg" width={500} height={400} className="rounded-lg shadow-lg" />
                         </div>
-                        <div className="flex-1 text-center sm:text-left mt-10 md:mt-0 px-0 md:px-6">
+                        <div className="flex-1 text-center sm:text-left mt-10 md:mt-0 px-0 md:px-6" data-aos="fade-down">
                                 <h2 className="text-3xl font-bold mb-2 text-light-color-Font dark:text-dark-color-Font">Cool Learning</h2>
                                 <p className="text-base mb-4 text-light-color-Font dark:text-dark-color-Font">
                                         Our goal in establishing Cool Learning is that anyone with any background and knowledge can easily enter the beautiful and lucrative world of programming.
